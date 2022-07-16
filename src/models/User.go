1fcdd1c4bd4694d8e7e2f4544425c35699a334b4
trantinh276 <tinhtranngoc@itrvn.com>
2022-07-17 03:05:40 +0700

    init

--- src/models/User.go
+++ src/models/User.go
@@ -1,9 +1,9 @@
 package models
 
-
 type User struct {
 	Id string `json:"_id"`
 	FirstName string `json:"firstName"`
 	LastName string `json:"lastName"`
 	Email string `json:"email"`
+	Password string `json."password`
 }
