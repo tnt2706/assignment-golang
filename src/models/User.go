f7784042f95a652123439c5998802ac4e508e949
Tran Tinh <tinhtranngoc@itrvn.com>
2022-07-18 11:32:00 +0700

    refactor

--- src/models/User.go
+++ src/models/User.go
@@ -1,12 +1,11 @@
 package models
 
-
 type User struct {
-	FirstName string `json:"firstName" validate:"required"`
-	LastName string `json:"lastName" validate:"required"`
-	Email string `json:"email" validate:"required"`
-	Salt int32 `json."salt" validate:"required`
-	Hash string `json."hash" validate:"required`
-	Roles []string `json."roles" validate:"required`
-	Status string `json."status" validate:"required`
+	FirstName string   `json:"firstName" validate:"required"`
+	LastName  string   `json:"lastName" validate:"required"`
+	Email     string   `json:"email" validate:"required"`
+	Salt      int32    `json."salt" validate:"required"`
+	Hash      string   `json."hash" validate:"required"`
+	Roles     []string `json."roles" validate:"required"`
+	Status    string   `json."status" validate:"required"`
 }
